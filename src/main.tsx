8cbb6cf178c23dc2692980b1a3b61069858a2010
slynch05 <slynch05@yahoo.com>
2025-10-22 11:31:11 -0400

    import telecom dash

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import TelecomInvoiceDashboard from "./TelecomInvoiceDashboard.tsx";
 
 Amplify.configure(outputs);
 
