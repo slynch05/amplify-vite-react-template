41b4fcc126e7c2365447559b03f755ae2a16c840
slynch05 <slynch05@yahoo.com>
2025-10-22 11:34:20 -0400

    revert to app

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,11 @@ import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
-import TelecomInvoiceDashboard from "./TelecomInvoiceDashboard.tsx";
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <TelecomInvoiceDashboard />
+    <App />
   </React.StrictMode>
 );
